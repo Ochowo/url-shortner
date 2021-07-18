df1c7eb095231b4a481dc4dc8181d580043c2ef0
senseseven <sense7creativa@gmail.com>
2021-07-18 20:58:20 +0100

    fix failing build

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from '../reducers';
+import counterReducer from '../features/counter/counterSlice';
 
-// eslint-disable-next-line import/prefer-default-export
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false,
-  }),
+  reducer: {
+    counter: counterReducer,
+  },
 });
+// export const store = configureStore({
+//   reducer: rootReducer,
+//   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+//     serializableCheck: false,
+//   }),
+// });
